95cfd9e19c85b6590ece1783575821c1f86ea53d
Strongadas <strongadas009@gmail.com>
2023-09-24 23:51:00 +0200

    added added .ignore

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
@@ -7,7 +8,13 @@ const nodemailer = require('nodemailer')
 const app = express()
 
 //mongoose.connect('mongodb://localhost:27017/TdSendersDb')
-mongoose.connect("mongodb+srv://Anacleto:Strongadas@cluster0.odsr23g.mongodb.net/teamDB")
+mongoose.connect(process.env.MONGO_DB,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
+})
 
 app.use(express.static('public'))
 app.set('view engine','ejs')
@@ -22,6 +29,16 @@ const senderSchema = new mongoose.Schema({
 
 const Sender = new mongoose.model('Sender', senderSchema)
 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port:456,
+    secure:true,
+    auth: {
+      user: process.env.GMAIL_EMAIL,
+      pass: process.env.GMAIL_PASSWORD,
+    },
+  });
+
 
 app.get('/',(req,res)=>{
     res.render('home')
@@ -33,58 +50,37 @@ app.get('/projects',(req,res)=>{
     res.render('projects')
 })
 
-app.post('/',(req,res)=>{
-
-    const name = req.body.name
-    const email = req.body.email
-    const message = req.body.message
-
-    const sender = new Sender({
-        sender:name,
-        email:email,
-        message:message
-    })
-
-   
-
-    const transporter = nodemailer.createTransport({
-        service:'gmail',
-        port:456,
-        secure:true,
-        auth:{
-            user: "teamdevelopers72@gmail.com",
-            pass:"tpqe yuyw rvnt cxmi"
-        }
-    })
-    const emailMessage = {
-        from:"Team Developers",
-        to: "teamdevelopers72@gmail.com",
-        subject: "Team Developers a new client " + name + " wants to get in touch with your Team",
-        text: email + " Said: " + message
-        
-
-    }
-    
-    transporter.sendMail(emailMessage,(err, info)=>{
-        
+app.post('/', async (req, res) => {
+    try {
+      const { name, email, message } = req.body;
+  
+      // Save sender data to MongoDB
+      const sender = new Sender({ name, email, message });
+      await sender.save((err)=>{
         if(err){
-            console.log('error sending email' , err)
+            console.log(err)
         }else{
-            console.log('Email sent:', info.response)
-            console.log(name)
-            
-            sender.save((err)=>{
-                if(err){
-                    console.log(err)
-                }else{
-                    console.log('saved to database')
-                    res.redirect('/')
-                }
-            })
+            console.log("saved to db")
         }
-    })
-})
-
+      });
+  
+      // Send email
+      const emailMessage = {
+        from: 'Team Developers',
+        to: 'teamdevelopers72@gmail.com',
+        subject: `New client ${name} wants to get in touch with your Team`,
+        text: `${email} said: ${message}`,
+      };
+  
+      await transporter.sendMail(emailMessage);
+      console.log('Email sent');
+  
+      res.redirect('/');
+    } catch (err) {
+      console.error('Error:', err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,(err)=>{
